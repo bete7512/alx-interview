964cb8ab1cb8194b3019e73d80f010d0a4736df2
bete7512 <betekbebe@gmail.com>
2022-12-22 09:17:40 +0300

    messages for pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+
+
+def pascal_triangle(nums):
+    """  pascal_triangle    """
+    if nums == 0:
+        return [[1]]
+    result = []
+    for i in range(0, nums - 1):
+        if len(result) == 0:
+            result.append([1])
+        holder = []
+        for j in range(0, i + 2):
+            if (j == 0 or j == (i + 1)):
+                holder.append(1)
+            else:
+                holder.append(result[i][j] + result[i][j-1])
+        result.append(holder)
+    print(result)
+    return result
