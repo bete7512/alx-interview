1bd0da7d925e3acf834c286d0c8a4f28686d29e0
bete7512 <betekbebe@gmail.com>
2023-01-05 16:24:24 +0300

    alx Interview Questions for Alx Software engnieering Program

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Lockboxes from ALX software Engineering program"""
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened"""
+    if type(boxes) is not list:
+        return False
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    for i in range(1, len(boxes)-1):
+        isLock = False
+        for j in range(0, len(boxes)):
+            if i in boxes[j] and j != i:
+                isLock = True
+                break
+        if isLock == False:
+            return False
+    return True
