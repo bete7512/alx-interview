c7ade46b5a68717ae65993396d32904ed0085202
bete7512 <betekbebe@gmail.com>
2023-01-12 21:52:53 +0300

    may be one day I will be a good competetive programmer

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,9 +4,15 @@ Main file for testing
 """
 def minOperations(n):
     """"minimum number of operations"""
-    h = 'H'
-    counter = 0
-    if(n <= 1):
-        return counter
-    copy = 'H'
+    if n <= 1:
+        return 0
+    i = 2
+    count = 0
+    while i <= n:
+        if n % i == 0:
+            count += i
+            n = n / i
+        else:
+            i += 1
+    return count
     
