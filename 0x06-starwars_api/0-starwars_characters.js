07e18a7d486209339886f584cd8ea130721988a9
bete7512 <betekbebe@gmail.com>
2023-02-07 23:54:19 +0300

    may be

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,34 @@
 #!/usr/bin/node
 
-const request = require('request');
-if (process.argv.length === 3) {
-  const id = process.argv[2];
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
-  const options = {
-    url: url,
-    json: true
-  };
-  request(options, async (err, res, body) => {
-    if (err) {
-      console.log(err);
-    } else {
-      for (const char of body.characters) {
-        const ret = async () => {
-          return new Promise((resolve, reject) => {
-            request(char, options, function (error, res, body) {
-              if (error) {
-                console.log(error);
-              } else {
-                resolve(body.name);
-              }
+const request = require("request");
+async function doTheTask() {
+  if (process.argv.length === 3) {
+    const id = process.argv[2];
+    const url = "https://swapi-api.alx-tools.com/api/films/" + id;
+    const options = {
+      url: url,
+      json: true,
+    };
+    request(options, async (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        for (const char of body.characters) {
+          const ret = async () => {
+            return new Promise((resolve, reject) => {
+              request(char, options, function (error, res, body) {
+                if (error) {
+                  console.log(error);
+                } else {
+                  resolve(body.name);
+                }
+              });
             });
-          });
-        };
-        console.log(await ret());
+          };
+          console.log(await ret());
+        }
       }
-    }
-  });
+    });
+  }
 }
+doTheTask();
