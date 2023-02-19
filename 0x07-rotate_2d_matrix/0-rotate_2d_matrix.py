d1cb24100dcd14efc649b3b5217e71dc7a5c72d8
bete7512 <betekbebe@gmail.com>
2023-02-19 21:02:10 +0300

    say me congratulation all github copilots are working

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+def rotate_2d_matrix(matrix):
+    """ Rotate 2D Matrix"""
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(len(matrix)):
+        matrix[i].reverse()
